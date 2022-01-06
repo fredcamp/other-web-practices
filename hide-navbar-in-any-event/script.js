fed26399051fb5264821fbebd91130c150e948c1
Fred Campo <87060464+fredcamp@users.noreply.github.com>
2022-01-06 10:44:09 +0800

    Add files via upload

--- hide-navbar-in-any-event/script.js
+++ hide-navbar-in-any-event/script.js
@@ -1,47 +1,61 @@
+const dropButtons = document.querySelectorAll(".dropbtn");
+const dropDowns = document.querySelectorAll(".dropdowns");
+const toggler = document.querySelector(".toggle");
 const nav = document.querySelector("nav");
-const navSize = nav.getBoundingClientRect()["height"];
-const section = document.querySelector("section");
-const subMenuList = document.querySelectorAll(".nav-menu-list");
-const subMenu = document.querySelectorAll(".sub-menu");
-const dropDowns = document.querySelectorAll(".nav-menu-list > a");
-let previousPosY = window.pageYOffset;
+const navbar = document.querySelector(".navbar-container");
+const navbarHeight = nav.getBoundingClientRect()["height"];
+const main = document.querySelector("main");
+let prevPosY = window.scrollY;
 
-section.style.marginTop = `${navSize}px`;
+const removeDropdownActiveState = () => {
+  dropButtons.forEach((button) => {
+    const dropdown = button.nextElementSibling;
+    dropdown.classList.remove("active");
+    button.classList.remove("active");
+  });
+};
 
 window.addEventListener("scroll", (e) => {
-  dropDowns.forEach((item) => {
-    if (item.classList.contains("active")) {
-      item.classList.remove("active");
+  const currPosY = window.scrollY;
+
+  if (!window.matchMedia("max-width:700px").matches) {
+    console.log("working");
+    if (currPosY > prevPosY) {
+      nav.style.top = `-${navbarHeight}px`;
+    } else {
+      nav.style.top = 0;
     }
-  });
-  const currentPosY = window.pageYOffset;
-
-  if (previousPosY > currentPosY) {
-    nav.style.top = 0;
-  } else {
-    nav.style.top = `-${navSize}px`;
+    prevPosY = currPosY;
   }
-
-  previousPosY = currentPosY;
 });
 
-subMenuList.forEach((item, index) => {
-  item.addEventListener("click", (e) => {
-    dropDowns.forEach((item, index2) => {
-      if (item.classList.contains("active") && index !== index2) {
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("active");
+  main.classList.toggle("active");
+});
+
+dropButtons.forEach((button, buttonIndex) => {
+  button.addEventListener("click", (e) => {
+    const dropdownBtn = e.target.nextElementSibling;
+    dropButtons.forEach((item, index) => {
+      if (item.classList.contains("active") && buttonIndex !== index) {
+        const dropdown = item.nextElementSibling;
         item.classList.remove("active");
+        dropdown.classList.remove("active");
       }
     });
+
     e.target.classList.toggle("active");
+    dropdownBtn.classList.toggle("active");
   });
 });
 
 window.addEventListener("click", (e) => {
-  if (!e.target.matches(".nav-menu-list > a")) {
-    dropDowns.forEach((item) => {
-      if (item.classList.contains("active")) {
-        item.classList.remove("active");
-      }
-    });
+  if (!e.target.matches(".dropbtn")) {
+    removeDropdownActiveState();
   }
+});
+
+window.addEventListener("scroll", (e) => {
+  removeDropdownActiveState();
 });
